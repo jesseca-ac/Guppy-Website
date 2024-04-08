72bccb797d4e8ae16112868fdd22c12eb4782c92
jesseca-ac <jesseca.ac@gmail.com>
2024-04-08 21:55:55 +0800

    Added chat widget, header, footer, reviews in components

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+document.getElementById('footer').innerHTML = `
+    <hr>
+    <p>© 2024 &middot; Built with love &hearts; by the beautiful people of the Philippines &middot; All rights reserved.</p>
+    <div>
+        <a href="https://www.facebook.com/guppyonestop" target="_blank"><img class="no-border-no-shadow" src="images/facebook.svg" /></a>
+        <a href="https://www.instagram.com/guppyonestop" target="_blank"><img class="no-border-no-shadow" src="images/instagram.svg" /></a>
+        <a href="https://www.youtube.com/@guppyonestop" target="_blank"><img class="no-border-no-shadow" src="images/youtube.svg" /></a>
+    </div>
+`
