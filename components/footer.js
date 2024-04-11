c76151ca8411c431239c336cd32dd1815a8a93a8
jesseca-ac <jesseca.ac@gmail.com>
2024-04-11 21:24:02 +0800

    created Rates page, added operating hours in footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,8 @@
+import('https://static.elfsight.com/platform/platform.js');
+
 document.getElementById('footer').innerHTML = `
     <hr>
+    <div class="elfsight-app-491386e6-2c8b-49eb-859b-a6014f08ab8d" data-elfsight-app-lazy></div>
     <p>© 2024 &middot; Built with love &hearts; by the beautiful people of the Philippines &middot; All rights reserved.</p>
     <div>
         <a href="https://www.facebook.com/guppyonestop" target="_blank"><img class="no-border-no-shadow" src="/images/facebook.svg" /></a>
--- components/header.js
+++ components/header.js
@@ -6,6 +6,7 @@ document.getElementById('header').innerHTML = `
         <a class="button" href="/index.html">Home</a>
         <a class="button" href="/pages/strollers.html">Our Strollers</a>
         <a class="button" href="/pages/about.html">Who We Are</a>
+        <a class="button" href="/pages/rates.html">Rates</a>
         <a class="button" href="/pages/faq.html">FAQ</a>
     </navbar>
 `;
