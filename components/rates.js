62e6e1b1007ed8fa06159484a7ad8b869e0839f4
jesseca-ac <jesseca.ac@gmail.com>
2024-04-12 23:28:35 +0800

    added background color, borders in main page contents, added who we are

---
+++ components/rates.js
@@ -0,0 +1,23 @@
+const rates = import('./rates.json');
+
+console.log(rates)
+
+// rates.forEach(rate => {
+//     document.getElementById('rates-table').innerHTML = `
+//         <div>
+//             <h3>${rate}</h3>
+//             <p>${rate} for 6 Hours</p>
+//             <p>${rate} for 24 Hours</p>
+//         </div>
+//     `;
+// });
+
+// rates.array.forEach(rate => {
+//     document.getElementById('rates-table').innerHTML = `
+//       <div>
+//            <h3>${rate}</h3>
+//            <p>${rate} for 6 Hours</p>
+//            <p>${rate} for 24 Hours</p>
+//        </div>
+//    `;
+// });
